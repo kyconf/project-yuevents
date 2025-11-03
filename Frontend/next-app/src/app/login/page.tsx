79defb905203d80e7cce2121b850e4bfe338877e
Trung Duc Pham <tduc@my.yorku.ca>
2025-11-03 13:17:11 -0500

    Remove duplicate folder Frontend

--- Frontend/next-app/src/app/login/page.tsx
+++
@@ -1,4 +0,0 @@
-import LogIn from "@/app/components/LogIn";
-export default function loginPage() {
-  return <LogIn></LogIn>;
-}
--- Frontend/next-app/src/app/signup/page.tsx
+++
@@ -1,4 +0,0 @@
-import SignUp from "@/app/components/SignUp";
-export default function signupPage() {
-  return <SignUp></SignUp>;
-}
